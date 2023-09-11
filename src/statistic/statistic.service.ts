7e60155f1b91c724965eef816e6e55316de28ae7
DiOnEasy <d11062002d@gmail.com>
2023-09-11 14:40:42 +0300

    added statistic (need to be finalized)

--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
-export class StatisticService {}
+export class StatisticService {
+	constructor(
+		private prisma: PrismaService,
+		private userService: UserService
+	) {}
+
+
+
+	async getMainStatistic(userId: number) {
+		const user = await this.userService.byId(userId, {
+			orders: {
+				select: {
+					items: true
+				}
+			},
+			reviews: true
+		})
+
+    //    return user.orders
+
+		return [
+			{
+				name: 'Orders',
+				value: user.orders.length
+			},{
+				name: 'Reviews',
+				value: user.reviews.length
+			},{
+				name: 'Favorites',
+				value: user.favorites.length
+			},{
+				name: 'Total amounts',
+				value: 1000
+			},
+		]
+	}
+}
