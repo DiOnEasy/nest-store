500d432349afdd7f7fd858fec67ca67cb0de7eb8
DiOnEasy <d11062002d@gmail.com>
2023-09-11 15:04:20 +0300

    added orders

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { OrderService } from './order.service';
+import { Controller, Get } from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { OrderService } from './order.service'
 
-@Controller('order')
+@Controller('orders')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+	constructor(private readonly orderService: OrderService) {}
+	@Auth()
+	@Get()
+	getAllOrders(@CurrentUser('id') id: number) {
+		return this.orderService.getAllOrders(id)
+	}
 }
